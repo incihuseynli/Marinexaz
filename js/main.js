81db86427d680bd0f2fbdfd5ace83edea4df87e8
incihuseynli <inci.huseynli0@gmail.com>
2023-09-12 22:19:35 +0400

    Updated

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Hamburger Menu
+const mobileMenu = document.querySelector('.menu');
+const hamburgerMenu = document.getElementById("hamburgerMenu");
+const closeBtn = document.getElementById("closeBtn");
+
+function openMenu(){
+    mobileMenu.style.transform = "translateX(0%)";
+}
+function closeMenu(){
+    mobileMenu.style.transform = "translateX(-200%)";
+}
