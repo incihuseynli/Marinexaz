e87d32012cc63d67055c9dae1d1fd9f2dc47c65d
incihuseynli <inci.huseynli0@gmail.com>
2023-10-09 11:58:47 +0400

    css and js added

--- js/local_tariffs.js
+++ js/local_tariffs.js
@@ -2,57 +2,54 @@ const priceWeights = document.querySelector(".head");
 const pricesRows = document.querySelectorAll(".row p");
 const contentsContainer = document.querySelector(".contents");
 
-const SHIPPING_PRICES_URL = "http://localhost:3001/shippingPrices?&_limit=6";
+const SHIPPING_PRICES_URL = "http://localhost:3001/places";
 
 fetch(SHIPPING_PRICES_URL)
-  .then((response) => {
-    return response.json();
-  })
+  .then((response) => response.json())
   .then((prices) => {
-    prices.forEach(({ tariff, forLiquids }) => {
-      pricesRows.forEach((pricesRow) => {
-        pricesRow += `<p>${forLiquids}</p>`;
-        let priceTag = pricesRow.innerHTML;
-
-        contentsContainer.innerHTML += `
-          <div class="content">
-            <div class="tariffTable">
-              <div class="table">
-                <div class="head">
-                  <p>${tariff}</p>
-                </div>
-                <div class="sort">
-                  <h4 class="subtitle">Adi məhsul</h4>
-                  <div class="row">
-                    ${priceTag}
+    prices.forEach(({ tariffs, id }) => {
+      const adiTag = tariffs.adi.reduce((a, b) => `${a}<p>${b}</p>`, "");
+      const mayeTag = tariffs.maye.reduce((a, b) => `${a}<p>${b}</p>`, "");
+      contentsContainer.innerHTML += `
+            <div class="content" id="${id}-content">
+              <div class="tariffTable">
+                <div class="table">
+                  <div class="head">
+                  <p>0.00 kg - 0.10 kg</p>
+                  <p>0.10 kg - 0.25 kg</p>
+                  <p>0.25 kg - 0.50 kg</p>
+                  <p>0.50 kg - 0.75 kg</p>
+                  <p>0.75 kg - 1.00 kg</p>
+                  <p>1.00 kg və üzəri</p>
                   </div>
-                </div>
-                <div class="sort">
-                  <h4 class="subtitle">Maye</h4>
-                  <div class="row">
-                  ${priceTag}
+                  <div class="sort">
+                    <h4 class="subtitle">Adi məhsul</h4>
+                    <div class="row">
+                      ${adiTag}
+                    </div>
+                  </div>
+                  <div class="sort">
+                    <h4 class="subtitle">Maye</h4>
+                    <div class="row">
+                    ${mayeTag}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-    `;
-      });
+      `;
     });
   });
 
-const tabs = document.querySelectorAll(".tab-btn");
-const contents = document.querySelectorAll(".content");
 
-tabs.forEach((tab, index) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((tab) => {
-      tab.classList.remove("active");
-    });
-    tab.classList.add("active");
-    contents.forEach((content) => {
-      content.classList.remove("active");
-    });
-    contents[index].classList.add("active");
+function tabClick(e) {
+  const contents = document.querySelectorAll(".content");
+  const tabs = document.querySelectorAll(".tab-btn");
+
+  tabs.forEach((tab, index) => {
+    tab.classList.remove("active");
+    contents[index].classList.remove("active");
   });
-});
+  document.getElementById(`${e.target.id}-content`).classList.add("active");
+  e.target.classList.add("active");
+}
--- js/news.js
+++ js/news.js
@@ -35,75 +35,67 @@ fetch(NEWS_URL)
     });
   });
 
-  // ============================================
-let newsCardList = document.querySelectorAll(".card")
+// ============================================
+let newsCardList = document.querySelectorAll(".card");
 const cards = document.querySelector(".cards");
 
+function showNews() {
+  // Starting value
+  let start = limit * (currentPage - 1);
+  // Ending value
+  let end = limit * currentPage - 1;
+  newsCardList.forEach((item, num) => {
+    if (num >= start && num <= end) {
+      item.style.display = "block";
+      // console.log(item);
+    } else {
+      item.style.display = "none";
+    }
+  });
+  getNewsPages();
+}
+showNews();
 
-  function showNews() {
-    // Starting value
-    let start = limit * (currentPage - 1);
-    // Ending value
-    let end = limit * currentPage - 1;
-    newsCardList.forEach((item, num) => {
-      if (num >= start && num <= end) {
-        item.style.display = "block";
-        // console.log(item);
-      } else {
-        item.style.display = "none";
-      }
-    });
-    getNewsPages();
+function getNewsPages() {
+  let count = Math.ceil(newsCardList.length / limit);
+  document.querySelector(".pagination").innerHTML = "";
+  if (currentPage != 1) {
+    let prev = document.createElement("li");
+    prev.classList.add("prev");
+    prev.setAttribute("onclick", "changeNewsPage(" + (currentPage - 1) + ")");
+    document.querySelector(".pagination").appendChild(prev);
+  } else {
+    let prev = document.createElement("li");
+    prev.classList.add("prev");
+    prev.setAttribute("disabled", "");
+    document.querySelector(".pagination").appendChild(prev);
+    prev.style.cursor = "not-allowed";
   }
+  for (i = 1; i <= count; i++) {
+    let newPage = document.createElement("li");
+    newPage.classList.add("page-number");
+    newPage.innerText = i;
+    if (i == currentPage) {
+      newPage.classList.add("active");
+    }
+    newPage.setAttribute("onclick", "changeNewsPage(" + i + ")");
+    document.querySelector(".pagination").appendChild(newPage);
+  }
+  if (currentPage != count) {
+    let next = document.createElement("li");
+    next.classList.add("next");
+    next.setAttribute("onclick", "changeNewsPage(" + (currentPage + 1) + ")");
+    document.querySelector(".pagination").appendChild(next);
+  } else {
+    let next = document.createElement("li");
+    next.classList.add("next");
+    next.setAttribute("disabled", "");
+    document.querySelector(".pagination").appendChild(next);
+    next.style.cursor = "not-allowed";
+  }
+}
+
+function changeNewsPage(i) {
+  currentPage = i;
   showNews();
-  
-  function getNewsPages() {
-    let count = Math.ceil(newsCardList.length / limit);
-    document.querySelector(".pagination").innerHTML = "";
-    if (currentPage != 1) {
-      let prev = document.createElement("li");
-      prev.classList.add("prev");
-      prev.setAttribute(
-        "onclick",
-        "changeNewsPage(" + (currentPage - 1) + ")"
-      );
-      document.querySelector(".pagination").appendChild(prev);
-    } else {
-      let prev = document.createElement("li");
-      prev.classList.add("prev");
-      prev.setAttribute("disabled", "");
-      document.querySelector(".pagination").appendChild(prev);
-      prev.style.cursor = "not-allowed";
-    }
-    for (i = 1; i <= count; i++) {
-      let newPage = document.createElement("li");
-      newPage.classList.add("page-number");
-      newPage.innerText = i;
-      if (i == currentPage) {
-        newPage.classList.add("active");
-      }
-      newPage.setAttribute("onclick", "changeNewsPage(" + i + ")");
-      document.querySelector(".pagination").appendChild(newPage);
-    }
-    if (currentPage != count) {
-      let next = document.createElement("li");
-      next.classList.add("next");
-      next.setAttribute(
-        "onclick",
-        "changeNewsPage(" + (currentPage + 1) + ")"
-      );
-      document.querySelector(".pagination").appendChild(next);
-    } else {
-      let next = document.createElement("li");
-      next.classList.add("next");
-      next.setAttribute("disabled", "");
-      document.querySelector(".pagination").appendChild(next);
-      next.style.cursor = "not-allowed";
-    }
-  }
-  
-  function changeNewsPage(i) {
-    currentPage = i;
-    showNews();
-  }
-  
+}
